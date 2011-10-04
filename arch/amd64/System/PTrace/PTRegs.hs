d395edcf6e69102b5704ce4a9a95450098b72efe
Matthew Maurer <maurer@matthewmaurer.org>
2011-10-03 21:06:49 -0400

    Do proper per-arch compilation.

--- arch/amd64/System/PTrace/PTRegs.hs
+++ arch/amd64/System/PTrace/PTRegs.hs
@@ -1,21 +1,21 @@
-module System.PTrace.X86_64 where
+module System.PTrace.PTRegs where
 
 import Data.Word
 import Foreign.Storable
 import Foreign.CStorable
 import GHC.Generics
 
-
 -- PENDING CStorable merging into Storable, simplify this
 data PTRegs =
   PTRegs {r15,r14,r13,r12,rbp,rbx,r11,r10,r9,r8,rax,rcx,rdx,rsi,
           rdi,orig_rax,rip,cs,eflags,rsp,ss,fs_base,gs_base,ds,es,
-          fs,gs :: Word64} deriving Generic
+          fs,gs :: Word64} deriving (Generic, Show)
 
 instance CStorable PTRegs
 
+--TODO fix CStorable package to compute alignment better...
 instance Storable PTRegs where
-  peek      = cPeek
-  poke      = cPoke
-  sizeOf    = cSizeOf
+  peek        = cPeek
+  poke        = cPoke
+  sizeOf      = cSizeOf
   alignment _ = 256
